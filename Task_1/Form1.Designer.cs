6614e7de5b8e0044525892a883abde67f5e7b26d
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2024-05-12 18:27:32 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minInput = new System.Windows.Forms.TextBox();
+            this.maxInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startSimple = new System.Windows.Forms.Button();
+            this.stopSimple = new System.Windows.Forms.Button();
+            this.outputSimple = new System.Windows.Forms.TextBox();
+            this.startFib = new System.Windows.Forms.Button();
+            this.stopFib = new System.Windows.Forms.Button();
+            this.outputFib = new System.Windows.Forms.TextBox();
+            this.Restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Генерировать простые числа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(425, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(302, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Генерировать числа Фибоначчи";
+            // 
+            // minInput
+            // 
+            this.minInput.Location = new System.Drawing.Point(74, 88);
+            this.minInput.Name = "minInput";
+            this.minInput.Size = new System.Drawing.Size(87, 20);
+            this.minInput.TabIndex = 2;
+            this.minInput.TextChanged += new System.EventHandler(this.minInput_TextChanged);
+            // 
+            // maxInput
+            // 
+            this.maxInput.Location = new System.Drawing.Point(235, 88);
+            this.maxInput.Name = "maxInput";
+            this.maxInput.Size = new System.Drawing.Size(87, 20);
+            this.maxInput.TabIndex = 3;
+            this.maxInput.TextChanged += new System.EventHandler(this.maxInput_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ОТ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ДО";
+            // 
+            // startSimple
+            // 
+            this.startSimple.Location = new System.Drawing.Point(74, 142);
+            this.startSimple.Name = "startSimple";
+            this.startSimple.Size = new System.Drawing.Size(100, 32);
+            this.startSimple.TabIndex = 6;
+            this.startSimple.Text = "START";
+            this.startSimple.UseVisualStyleBackColor = true;
+            this.startSimple.Click += new System.EventHandler(this.startSimple_Click);
+            // 
+            // stopSimple
+            // 
+            this.stopSimple.Location = new System.Drawing.Point(208, 142);
+            this.stopSimple.Name = "stopSimple";
+            this.stopSimple.Size = new System.Drawing.Size(100, 32);
+            this.stopSimple.TabIndex = 7;
+            this.stopSimple.Text = "STOP";
+            this.stopSimple.UseVisualStyleBackColor = true;
+            this.stopSimple.Click += new System.EventHandler(this.stopSimple_Click);
+            // 
+            // outputSimple
+            // 
+            this.outputSimple.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputSimple.Location = new System.Drawing.Point(110, 206);
+            this.outputSimple.MaximumSize = new System.Drawing.Size(150, 40);
+            this.outputSimple.MinimumSize = new System.Drawing.Size(50, 40);
+            this.outputSimple.Name = "outputSimple";
+            this.outputSimple.ReadOnly = true;
+            this.outputSimple.Size = new System.Drawing.Size(150, 20);
+            this.outputSimple.TabIndex = 8;
+            // 
+            // startFib
+            // 
+            this.startFib.Location = new System.Drawing.Point(449, 88);
+            this.startFib.Name = "startFib";
+            this.startFib.Size = new System.Drawing.Size(100, 32);
+            this.startFib.TabIndex = 9;
+            this.startFib.Text = "START";
+            this.startFib.UseVisualStyleBackColor = true;
+            this.startFib.Click += new System.EventHandler(this.startFib_Click);
+            // 
+            // stopFib
+            // 
+            this.stopFib.Location = new System.Drawing.Point(596, 88);
+            this.stopFib.Name = "stopFib";
+            this.stopFib.Size = new System.Drawing.Size(100, 32);
+            this.stopFib.TabIndex = 10;
+            this.stopFib.Text = "STOP";
+            this.stopFib.UseVisualStyleBackColor = true;
+            this.stopFib.Click += new System.EventHandler(this.stopFib_Click);
+            // 
+            // outputFib
+            // 
+            this.outputFib.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputFib.Location = new System.Drawing.Point(497, 149);
+            this.outputFib.MaximumSize = new System.Drawing.Size(150, 40);
+            this.outputFib.MinimumSize = new System.Drawing.Size(50, 40);
+            this.outputFib.Name = "outputFib";
+            this.outputFib.ReadOnly = true;
+            this.outputFib.Size = new System.Drawing.Size(150, 20);
+            this.outputFib.TabIndex = 11;
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(327, 317);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(153, 52);
+            this.Restart.TabIndex = 12;
+            this.Restart.Text = "RESTART";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.outputFib);
+            this.Controls.Add(this.stopFib);
+            this.Controls.Add(this.startFib);
+            this.Controls.Add(this.outputSimple);
+            this.Controls.Add(this.stopSimple);
+            this.Controls.Add(this.startSimple);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maxInput);
+            this.Controls.Add(this.minInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox minInput;
+        private System.Windows.Forms.TextBox maxInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button startSimple;
+        private System.Windows.Forms.Button stopSimple;
+        private System.Windows.Forms.TextBox outputSimple;
+        private System.Windows.Forms.Button startFib;
+        private System.Windows.Forms.Button stopFib;
+        private System.Windows.Forms.TextBox outputFib;
+        private System.Windows.Forms.Button Restart;
+    }
+}
+
---
+++ Task_1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        //public CancellationTokenSource cancelTokenSource;
+        //public CancellationToken token;
+        //public CancellationTokenSource cancelTokenSource2;
+        //public CancellationToken token2;
+        ManualResetEvent resetEvent1 = new ManualResetEvent(true);
+        public ThreadStart ts1;
+        public Thread t1;
+
+        ManualResetEvent resetEvent2 = new ManualResetEvent(true);
+        public ThreadStart ts2;
+        public Thread t2;
+
+        public int a;
+        public int b;
+        public int c;
+       public int current = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            ts1 = new ThreadStart(FindSimple);
+            t1 = new Thread(ts1);
+
+            ts2 = new ThreadStart(FindFib);
+            t2 = new Thread(ts2);
+        }
+
+        private void minInput_TextChanged(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(minInput.Text);
+        }
+
+        private void startSimple_Click(object sender, EventArgs e)
+        {
+            //cancelTokenSource = new CancellationTokenSource();
+            //token = cancelTokenSource.Token;
+            //
+           // resetEvent1 = new ManualResetEvent(true);
+            resetEvent1.Set();
+            if(!t1.IsAlive)
+            {
+               
+                t1.Start();
+            }
+              
+            //await Task.Run(() => FindSimple(), token);
+        }
+
+        private void maxInput_TextChanged(object sender, EventArgs e)
+        {
+            b = Convert.ToInt32(maxInput.Text);
+        }
+       public void FindSimple()
+        {
+           
+            if (minInput.Text==String.Empty) a = 2;
+         
+            if(maxInput.Text==String.Empty) b = int.MaxValue;
+            for(int i=a;  i<b; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    Invoke(new Action(()=> outputSimple.Text = i.ToString()));
+                    a = i;
+                    resetEvent1.WaitOne();
+                    Thread.Sleep(500);
+                   //if(token.IsCancellationRequested) break;
+                }
+            }
+        }
+        private static bool IsPrimeNumber(int number)
+        {
+            int sqrtNumber = (int)(Math.Sqrt(number));
+            for (int i = 2; i <= sqrtNumber; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public void FindFib()
+        {
+            int prev = 0;
+            
+            int temp;
+
+            for (int i = 0; i <= int.MaxValue; i++)
+            {
+                Invoke(new Action(() => outputFib.Text = current.ToString()));
+                //a = i;
+                temp = current;
+                current = current + prev;
+                prev = temp;
+                resetEvent2.WaitOne();
+                Thread.Sleep(500);
+               
+                //if (token2.IsCancellationRequested) break;
+
+            }
+        }
+        private void stopSimple_Click(object sender, EventArgs e)
+        {
+            resetEvent1.Reset();
+            //t1.Abort(); 
+            //cancelTokenSource.Cancel();
+            //startSimple_Click
+        }
+
+        private void Restart_Click(object sender, EventArgs e)//TODO
+        {
+            //cancelTokenSource.Cancel();
+            a = 0; b=0;
+            current = 1;
+           // outputFib.Text = resetEvent1.Handle.ToString();
+           
+            resetEvent1.Close();
+            resetEvent2.Close();
+            resetEvent1.Dispose();
+            resetEvent2.Dispose();
+            ts1 = new ThreadStart(FindSimple);
+            t1 = new Thread(ts1);
+           
+
+            ts2 = new ThreadStart(FindFib);
+            t2 = new Thread(ts2);
+
+            resetEvent1 = new ManualResetEvent(true);
+            resetEvent2 = new ManualResetEvent(true);
+            //t1.Abort();
+            //t2.Abort();
+
+        }
+
+        private void startFib_Click(object sender, EventArgs e)
+        {
+            resetEvent2.Set();
+            if (!t2.IsAlive) t2.Start();
+          
+            //cancelTokenSource2 = new CancellationTokenSource();
+            //token2 = cancelTokenSource.Token;
+            //
+            //ts1 = new ThreadStart(FindSimple);
+            // t1 = new Thread(ts1);
+            // t1.Start();
+            //await Task.Run(() => FindFib(), token2);
+        }
+
+        private void stopFib_Click(object sender, EventArgs e)
+        {
+            resetEvent2.Reset();
+            //cancelTokenSource2.Cancel();
+        }
+    }
+}
